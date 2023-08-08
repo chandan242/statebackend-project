30cf466ae785cd22946019a9c0e809332b165afa
Chandan <chandankrgupta9262@gmail.com>
2023-08-08 12:35:40 +0530

    form validation and other task

--- src/components/multiCardWidget.jsx
+++ src/components/multiCardWidget.jsx
@@ -12,37 +12,37 @@ export const MultiCardWidgets = () => {
           count: "2262",
           description: "Rash Turning",
           bgcolor: "#a7a8a8",
-          icon: <GiReturnArrow  size={35}/>
+          icon: <GiReturnArrow  size={45}/>
         },
         {
           count: "10",
           description: "Over Speed",
           bgcolor: "#f5717d",
-          icon: <MdSpeed size={35}/>
+          icon: <MdSpeed size={45}/>
         },
         {
           count: "69",
           description: "Harsh Breaking",
           bgcolor: "#8df2a4",
-          icon: <VscDebugBreakpointUnsupported size={35}/>
+          icon: <VscDebugBreakpointUnsupported size={57}/>
         },
         {
           count: "86",
           description: "Harsh Acceleration",
           bgcolor: "#f0a667",
-          icon: <FaStackOverflow size={35}/>
+          icon: <FaStackOverflow size={45}/>
         },
         {
           count: "11",
           description: "Device Tampered",
           bgcolor: "#7ee7f7",
-          icon: <SiTampermonkey size={35}/>
+          icon: <SiTampermonkey size={43}/>
         },
         {
             count: "8",
             description: "Low Battery",
             bgcolor: "#c78cfa",
-            icon: <BiSolidBatteryLow size={35}/>
+            icon: <BiSolidBatteryLow size={45}/>
         }
       ];
       
